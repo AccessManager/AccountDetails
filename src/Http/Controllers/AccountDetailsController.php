66a83216dca81ca0fdefab97c49a27887ec25345
Rahul makhija <rahulmkhj@gmail.com>
2017-08-03 19:57:38 +0530

    added methods to AccountDetailsController

--- src/Http/Controllers/AccountDetailsController.php
+++ src/Http/Controllers/AccountDetailsController.php
@@ -7,5 +7,14 @@ use AccessManager\Base\Http\Controller\AdminBaseController;
 
 class AccountDetailsController extends AdminBaseController
 {
+    public function getSubscriptions()
+    {
+        $subscriptions = [];
+        return view('AccountDetails::subscriptions', compact('subscriptions'));
+    }
 
+    public function getDemographics()
+    {
+        return view("AccountDetails::demographics");
+    }
 }
