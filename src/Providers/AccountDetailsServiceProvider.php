96fe7bd8fa122d53a460e46ccc070114bb56d5f6
Rahul makhija <rahulmkhj@gmail.com>
2017-09-21 19:54:22 +0530

    updated ServiceProvider as per updated package files.

--- src/Providers/AccountDetailsServiceProvider.php
+++ src/Providers/AccountDetailsServiceProvider.php
@@ -3,26 +3,56 @@
 namespace AccessManager\AccountDetails\Providers;
 
 
+use AccessManager\AccountDetails\AccountSubscription\Models\AccountSubscription;
 use AccessManager\Accounts\Models\Account;
 use Illuminate\Support\ServiceProvider;
 
+/**
+ * Class AccountDetailsServiceProvider
+ * @package AccessManager\AccountDetails
+ */
 class AccountDetailsServiceProvider extends ServiceProvider
 {
+    /**
+     * here we register package's migrations, views & routes.
+     */
     public function boot()
     {
-        $this->loadRoutesFrom( __DIR__ . "/../Routes/web.php");
-        $this->loadViewsFrom( __DIR__ . "/../Views", "AccountDetails");
-
-        $this->composeView();
+        $this->bootAccountDetails();
+        $this->bootAccountSubscriptions();
+        $this->loadMigrationsFrom( __DIR__ . "/../Database/Migrations");
     }
 
-    protected function composeView()
+    protected function bootAccountDetails()
     {
+        $this->loadRoutesFrom( __DIR__ . "/../AccountDetails/Routes/web.php");
+        $this->loadViewsFrom( __DIR__ . "/../AccountDetails/Views", "AccountDetails");
+        $this->composeAccountDetailsView();
+    }
 
-        view()->composer('AccountDetails::partials.account-info', function($view){
+    protected function bootAccountSubscriptions()
+    {
+        $this->loadRoutesFrom( __DIR__ . "/../AccountSubscription/Routes/web.php");
+        $this->loadViewsFrom( __DIR__ . "/../AccountSubscription/Views", "AccountSubscription");
+        $this->composeAccountSubscriptionView();
+    }
+
+    protected function composeAccountDetailsView()
+    {
+        view()->composer('AccountDetails::layout', function($view){
             $account = Account::where('username', request()->segment(2))->firstOrFail();
 
             $view->with(compact('account'));
+        });
+    }
+
+    protected function composeAccountSubscriptionView()
+    {
+        view()->composer('AccountSubscription::layout', function($view){
+            $account = Account::where('username', request()->segment(2))->firstOrFail();
+            $subscription = AccountSubscription::where('username', request()->segment(4))->firstOrFail();
+
+            $view->with(compact('account', 'subscription'));
         });
     }
 }
