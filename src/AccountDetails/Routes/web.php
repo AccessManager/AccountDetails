47f50c0799f34d314d89b4db103e429504ff43da
Rahul makhija <rahulmkhj@gmail.com>
2017-09-21 19:49:29 +0530

    separate web.php to hold routes for account details section only.

---
+++ src/AccountDetails/Routes/web.php
@@ -0,0 +1,45 @@
+<?php
+Route::group([
+    'namespace'     =>      'AccessManager\AccountDetails\AccountDetails\Http\Controllers',
+    'prefix'        =>      'accounts/{username}',
+    'middleware'    =>      'auth',
+], function (){
+
+    Route::get('/', [
+        'as'    =>  'account.details',
+        'uses'  =>  function(){
+                        return redirect()->route('account.subscriptions', request()->segment(2));
+                    }
+    ]);
+
+    Route::get('/demographics', [
+        'as'    =>  'account.demographics',
+        'uses'  =>  'AccountDetailsController@getDemographics',
+    ]);
+
+    Route::get('/subscriptions', [
+        'as'    =>  'account.subscriptions',
+        'uses'  =>  'AccountDetailsController@getSubscriptions',
+    ]);
+
+    Route::get('/subscriptions/add', [
+        'as'    =>  'account.subscriptions.add',
+        'uses'  =>  'AccountDetailsController@getAddSubscription',
+    ]);
+
+    Route::post('/subscriptions/add', [
+        'as'    =>  'account.subscriptions.add.post',
+        'uses'  =>  'AccountDetailsController@postAddSubscription',
+    ]);
+
+    Route::get('/change-password', [
+        'as'    =>  'account.change-password',
+        'uses'  =>  'AccountDetailsController@getChangePassword',
+    ]);
+
+    Route::post('/change-password', [
+        'as'    =>  'account.change-password.post',
+        'uses'  =>  'AccountDetailsController@getChangePassword',
+    ]);
+
+});
