691f25ac5a17490be02fe15f7473a4585662d3f3
Rahul makhija <rahulmkhj@gmail.com>
2017-09-21 19:53:53 +0530

    new PrepaidSubscriptionController to take care of prepaid subscriptions.

---
+++ src/AccountSubscription/Http/Controllers/PrepaidSubscriptionController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace AccessManager\AccountDetails\AccountSubscription\Http\Controllers;
+
+
+use AccessManager\AccountDetails\AccountSubscription\Models\PrepaidSubscription;
+use AccessManager\AccountDetails\AccountSubscription\Requests\ChangeSubscriptionPasswordRequest;
+
+class PrepaidSubscriptionController extends AccountSubscriptionsController
+{
+
+    public function getServices($accountUsername, $subscriptionUsername )
+    {
+        $subscription = PrepaidSubscription::where('username', $subscriptionUsername)->firstOrFail();
+        return view('AccountSubscription::prepaid.services', compact('subscription'));
+    }
+
+    public function getSessionHistory( $accountUsername, $subscriptionUsername )
+    {
+        return view('AccountSubscription::prepaid.session-history');
+    }
+
+    public function getRechargeHistory($accountUsername, $subscriptionUsername)
+    {
+        $subscription = PrepaidSubscription::where('username', $subscriptionUsername)->firstOrFail();
+        $vouchers = $subscription->vouchers()->paginate(5);
+        return view('AccountSubscription::prepaid.recharge-history', compact('vouchers'));
+    }
+
+    public function getChangePassword( $accountUsername, $subscriptionUsername )
+    {
+        return view('AccountSubscription::prepaid.change-password');
+    }
+
+    public function postChangePassword( $accountUsername, $subscriptionUsername, ChangeSubscriptionPasswordRequest $request )
+    {
+        try {
+            $subscription = PrepaidSubscription::where('username', $subscriptionUsername)->firstOrFail();
+            $subscription->password = $request->password;
+            $subscription->saveOrFail();
+            return back();
+        }
+        catch (\Exception $e)
+        {
+            dd($e->getMessage());
+        }
+    }
+
+}
