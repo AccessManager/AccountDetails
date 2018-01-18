f2575fcd02d6ad659f98bb6de6c624516747ca6a
Rahul makhija <rahulmkhj@gmail.com>
2018-01-18 17:19:23 +0530

    implemented view to change FreeSubscription password.

---
+++ src/AccountSubscription/Views/free/change-password.blade.php
@@ -0,0 +1,7 @@
+@extends("AccountSubscription::prepaid.layout")
+@section('tab-content')
+    {!! Form::open(['route'=>['account.subscriptions.free.change-password.post', request()->segment(2), request()->segment(4)],
+        'class'=>'form-horizontal']) !!}
+    @include("AccountSubscription::partials.change-password")
+    {!! Form::close() !!}
+@endsection
